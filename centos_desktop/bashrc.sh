8fc155ea182227c8d69aefad219dd076dd84b862
sc724 <kz724@outlook.com>
2015-11-18 20:43:58 +0000

    add centos package backup file

---
+++ centos_desktop/bashrc.sh
@@ -0,0 +1,13 @@
+alias rm='rm -i'
+alias cp='cp -i'
+alias mv='mv -i'
+
+# Source global definitions
+if [ -f /etc/bashrc ]; then
+	. /etc/bashrc
+fi
+
+export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.65-2.b17.el7_1.x86_64
+
+
+

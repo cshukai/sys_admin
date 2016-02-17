74e31666933766af9899e85af8a349a21837a6ba
sc724 <kz724@outlook.com>
2016-02-17 22:18:08 +0000

    add yum line for installation of R on centos

--- bigdata/hadoop.sh
+++ bigdata/hadoop.sh
@@ -1,1 +1,4 @@
-#installation
+#installation on mu centos-desktop
+export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.71-2.b15.el7_2.x86_64/
+export HADOOP_INSTALL=/home/shukai/Downloads/hadoop-2.7.2
+export PATH=$PATH:$HADOOP_INSTALL/bin:$HADOOP_INSTALL/sbin
--- centos_desktop/package.sh
+++ centos_desktop/package.sh
@@ -1,5 +1,7 @@
-yum install java-1.8.0-openjdk.x86_64
-yum install httpd
+sudo yum install java-1.8.0-openjdk.x86_64
+sudo yum install httpd
+sudo  yum install epel-release
+sudo  yum install R
 cd /home
 mkdir bigdata
 cd bigdata

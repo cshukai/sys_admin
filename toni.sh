dc45c87282c8ee0b8f990bc34042383f8ee2d6d3
sc724 <kz724@outlook.com>
2016-05-05 23:49:47 +0000

    add tophat

--- toni.sh
+++ toni.sh
@@ -3,3 +3,21 @@
 cd /opt/bio/picard-tools-2.1.1 
 nohup java -jar picard.jar FastqToSam FASTQ=/courses/7740_F2015/gwas_project/rna_seq/SRR611339.fastq OUTPUT=/courses/7740_F2015/gwas_project/rna_seq/SRR611339.bam  SAMPLE_NAME=gwas 
 
+# GC content
+read_GC.py -i SRR611339.bam -o gc_output
+# qc
+read_quality.py -i SRR611339.bam -o output
+
+
+#ValueError: file header is empty (mode='r') - is it SAM/BAM format?
+
+
+##############alignment
+/courses/INFOINST8005_F2015/Software/tophat/tophat2 -o tophat_out_611339  -p 5  
+-G  /courses/7740_F2015/gwas_project/ftp.ensemblgenomes.org/pub/plants/release-2
+2/gff3/zea_mays/Zea_mays.AGPv3.22.gff3    /courses/7740_F2015/gwas_project/rna_s
+eq/Zea_mays/Ensembl/AGPv3/Sequence/Bowtie2Index/genome    SRR611339.fastq.gz
+/courses/INFOINST8005_F2015/Software/tophat/tophat2 -o tophat_out_51010   -p 5  
+-G  /courses/7740_F2015/gwas_project/ftp.ensemblgenomes.org/pub/plants/release-2
+2/gff3/zea_mays/Zea_mays.AGPv3.22.gff3 /courses/7740_F2015/gwas_project/rna_seq/
+Zea_mays/Ensembl/AGPv3/Sequence/Bowtie2Index/genome   SRR651010.fastq.gz

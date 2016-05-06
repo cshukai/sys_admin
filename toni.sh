77d64c08a7f10f65e2f1b35fcbcef977ac1b1312
sc724 <kz724@outlook.com>
2016-05-06 20:22:57 +0000

    clip run

--- toni.sh
+++ toni.sh
@@ -1,21 +1,15 @@
-################RSeQc######################
-# convert fastq to sam
-#cd /opt/bio/picard-tools-2.1.1 
-#nohup java -jar picard.jar FastqToSam FASTQ=/courses/7740_F2015/gwas_project/rna_seq/SRR611339.fastq OUTPUT=/courses/7740_F2015/gwas_project/rna_seq/SRR611339.bam  SAMPLE_NAME=gwas 
+################RSeQc##########################################
 
 # GC content
-read_GC.py -i tophat_out_51010/accepted_hits.bam -o gc_output
-read_GC.py -i tophat_out_611339/accepted_hits.bam  -o output
+read_GC.py -i tophat_out_51010/accepted_hits.bam -o output_gc_51010
+read_GC.py -i tophat_out_611339/accepted_hits.bam  -o output_gc_611339 
 
 # qc
-read_quality.py -i tophat_out_51010/accepted_hits.bam -o gc_output
-read_quality.py -i tophat_out_611339/accepted_hits.bam  -o output
+read_quality.py -i tophat_out_51010/accepted_hits.bam -o output_qa_51010
+read_quality.py -i tophat_out_611339/accepted_hits.bam  -o output_qa_611339
 
 
-#ValueError: file header is empty (mode='r') - is it SAM/BAM format?
-
-
-##############alignment
+##############tophat alignment################################
 /courses/INFOINST8005_F2015/Software/tophat/tophat2 -o tophat_out_611339  -p 5  
 -G  /courses/7740_F2015/gwas_project/ftp.ensemblgenomes.org/pub/plants/release-2
 2/gff3/zea_mays/Zea_mays.AGPv3.22.gff3    /courses/7740_F2015/gwas_project/rna_s
